52a490d24d40d76b9a6cf858db9152763002a45f
wowik664 <w-jakubiec@wp.pl>
2025-03-22 22:41:31 +0100

    test i skrypt hotjar

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,27 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Navigation Buttons Test', function () {
+  it('Allows user to navigate slides using next and previous buttons', function () {
+    cy.visit('http://localhost:3000');
+
+    cy.get('.swiper-slide-active').then(($initialSlide) => {
+      const initialContent = $initialSlide.text();
+
+      cy.get('.swiper-button-next').click();
+      cy.wait(2000);
+
+      cy.get('.swiper-slide-active').should(($newSlide) => {
+        expect($newSlide.text()).not.to.eq(initialContent);
+      });
+
+      cy.get('.swiper-button-prev').click();
+      cy.wait(2000);
+
+      cy.get('.swiper-slide-active').should(($returnedSlide) => {
+        expect($returnedSlide.text()).to.eq(initialContent);
+      });
+    });
+  });
+});
